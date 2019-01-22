59b46323fb2d70e8d0fbd5c149362bbd1bd9a1ac
Shrijit Singh <shrijitsingh99@gmail.com>
2019-01-23 02:18:17 +0530

    Format code using clang-format

--- include/serial_parser/serial_parser.h
+++ include/serial_parser/serial_parser.h
@@ -1,15 +1,15 @@
+#include <functional>
 #include <iostream>
+#include <iterator>
+#include <memory>
 #include <string>
 #include <vector>
-#include <iterator>
-#include <functional>
-#include <memory>
 
 #include "serial/serial.h"
 
 namespace serial {
 class SerialParser {
-private:
+ private:
   Serial *serial;
   const uint8_t *start_delimitter;
   const uint8_t *end_delimmiter;
@@ -17,12 +17,17 @@ private:
   int end_delimmiter_length;
   int bytes_to_read;
 
-public:
+ public:
   SerialParser(){};
   ~SerialParser(){};
-  SerialParser(Serial &serial, std::string start_delimitter, std::string end_delimmiter, int bytes_to_read);
-  std::string parse(std::function<bool(uint8_t *, const uint8_t *)> start_function, std::function<bool(uint8_t *, const uint8_t *)> end_function);
+  SerialParser(Serial &serial, std::string start_delimitter,
+               std::string end_delimmiter, int bytes_to_read);
+  std::string parse(
+      std::function<bool(uint8_t *, const uint8_t *)> start_function,
+      std::function<bool(uint8_t *, const uint8_t *)> end_function);
   std::string get_parsed_string();
-  std::string get_parsed_string(std::function<bool(uint8_t *, const uint8_t *)> start_function, std::function<bool(uint8_t *, const uint8_t *)> end_function);
+  std::string get_parsed_string(
+      std::function<bool(uint8_t *, const uint8_t *)> start_function,
+      std::function<bool(uint8_t *, const uint8_t *)> end_function);
 };
-}
+}  // namespace serial
