73d3d412d0385ceb876c82a0239efde39990fbfd
Shrijit Singh <shrijitsingh99@gmail.com>
2019-04-07 00:38:12 +0530

    Fix pointer bug causing delimiter data to go out of scope

--- include/serial_parser/serial_parser.h
+++ include/serial_parser/serial_parser.h
@@ -4,12 +4,12 @@ namespace serial {
 using delimiter_function = bool(uint8_t*, const uint8_t*, size_t);
 class SerialParser {
  private:
-  Serial* serial;
-  const uint8_t* start_delimiter;
-  const uint8_t* end_delimiter;
-  size_t start_delimiter_length;
-  size_t end_delimiter_length;
-  int bytes_to_read;
+  Serial* serial_;
+  uint8_t* start_delimiter_;
+  uint8_t* end_delimiter_;
+  size_t start_delimiter_length_;
+  size_t end_delimiter_length_;
+  int bytes_to_read_;
 
  public:
   SerialParser() = default;
@@ -17,7 +17,8 @@ class SerialParser {
   SerialParser(Serial& serial, std::string start_delimiter,
                std::string end_delimiter, int bytes_to_read);
   std::string parse(delimiter_function* start_function,
-                    delimiter_function* end_function);
+                    delimiter_function* end_function);  // TODO: Use pass by
+                                                        // pointer not reference
   std::string get_parsed_string();
   std::string get_parsed_string(delimiter_function* start_function,
                                 delimiter_function* end_function);
--- src/serial_parser.cpp
+++ src/serial_parser.cpp
@@ -4,14 +4,14 @@ using namespace serial;
 
 SerialParser::SerialParser(Serial& serial, std::string start_delimiter,
                            std::string end_delimiter, int bytes_to_read) {
-  this->serial = &serial;
-  this->start_delimiter_length = strlen(start_delimiter.c_str());
-  this->end_delimiter_length = strlen(end_delimiter.c_str());
-  this->start_delimiter =
-      reinterpret_cast<const uint8_t*>(start_delimiter.data());
-  this->end_delimiter =
-      reinterpret_cast<const uint8_t*>(end_delimiter.data());
-  this->bytes_to_read = bytes_to_read;
+  serial_ = &serial;
+  start_delimiter_length_ = strlen(start_delimiter.c_str());
+  end_delimiter_length_ = strlen(end_delimiter.c_str());
+  start_delimiter_ = (uint8_t  *) malloc(start_delimiter.size());
+  end_delimiter_ = (uint8_t  *) malloc(end_delimiter.size());
+  memcpy(start_delimiter_, start_delimiter.data(), start_delimiter.size());
+  memcpy(end_delimiter_, end_delimiter.data(), end_delimiter.size());
+  bytes_to_read_ = bytes_to_read;
 }
 
 bool start_reading(uint8_t* start_buffer, const uint8_t* start_delimiter, size_t start_delimiter_length) {
@@ -36,30 +36,30 @@ std::string SerialParser::get_parsed_string(delimiter_function* start_function, 
 
 std::string SerialParser::parse(delimiter_function* start_function, delimiter_function* end_function) {
   std::string parsed_string;
-  uint8_t start_buffer[start_delimiter_length], end_buffer[end_delimiter_length];
+  uint8_t start_buffer[start_delimiter_length_], end_buffer[end_delimiter_length_];
   uint8_t c;
 
-  while (serial->available()) {
+  while (serial_->available()) {
     parsed_string = "";
-    c = (uint8_t)serial->read()[0];
+    c = (uint8_t)serial_->read()[0];
 
-    if (start_delimiter_length > 0) {
-      memmove(start_buffer, start_buffer + 1, start_delimiter_length - 1);
-      start_buffer[start_delimiter_length - 1] = c;
+    if (start_delimiter_length_ > 0) {
+      memmove(start_buffer, start_buffer + 1, start_delimiter_length_ - 1);
+      start_buffer[start_delimiter_length_ - 1] = c;
     }
 
-    if (!(*start_function)(start_buffer, start_delimiter,
-                          start_delimiter_length))
+    if (!(*start_function)(start_buffer, start_delimiter_,
+                          start_delimiter_length_))
       continue;
 
-    for (int i = 0; i < bytes_to_read; ++i) {
-      parsed_string += serial->read()[0];
+    for (int i = 0; i < bytes_to_read_; ++i) {
+      parsed_string += serial_->read()[0];
     }
 
-    for (int i = 0; i < end_delimiter_length; ++i)
-      end_buffer[i] = (uint8_t)serial->read()[0];
+    for (int i = 0; i < end_delimiter_length_; ++i)
+      end_buffer[i] = (uint8_t)serial_->read()[0];
 
-    if (!(*end_function)(end_buffer, end_delimiter, end_delimiter_length))
+    if (!(*end_function)(end_buffer, end_delimiter_, end_delimiter_length_))
       continue;
 
     return parsed_string;
